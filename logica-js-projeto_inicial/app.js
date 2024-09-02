2b7ad064aa8ab92a8077654ba7386a2c45e0e0d1
Rafa WG <110629098+rafawg@users.noreply.github.com>
2024-09-02 10:34:37 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,112 +1,49 @@
-
 alert('Boas vindas ao numero secreto');
-
-
-let
-numeroSecreto = 6;
-
-
+let numeroSecreto = 6;
 console.log(numeroSecreto);
-
-
-let
-chute;
-
-
-let
-tentativas = 1;
-
-
- 
-
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao numero Secreto
+while (chute != numeroSecreto) {
+     chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
 
-
-while
-(chute != numeroSecreto) {
-
-
- 
-  chute = prompt('Escolha um número entre 1 e 10');
-
-
- 
-  // se chute for igual ao número secreto
-
-
- 
-  if (chute == numeroSecreto) {
-
-
- 
-      break;
-
-
- 
-     
-
-
- 
-  } else {
-
-
- 
-      if (chute > numeroSecreto) {
-
-
- 
-      alert (`O numeroSecreto é menor que ${chute}`);
-
-
- 
-      } else {
-
-
- 
-      alert (`O numeroSecreto é maior que ${chute}`);
-
-
- 
-      }
-
-
- 
-  }   //tentativas = tentativas + 1;
-
-
- 
-      tentativas++;
-
-
+    } else {
+        if (chute > numeroSecreto) {
+        alert (`O numeroSecreto é menor que ${chute}`);
+        } else {
+        alert (`O numeroSecreto é maior que ${chute}`);
+        }
+    }   //tentativas = tentativas + 1;
+        tentativas++;
 }
-
-
- 
-
-
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-
-
-alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-
- 
-
-
-//if (tentativas > 1) {
-
-
-//  alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-
-
-//} else {
-
-
-// alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-
-
-//} tentativas++;}
-
-
-
+    
+    
+     
+    
+    
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    
+    
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+    
+    
+     
+    
+    
+    //if (tentativas > 1) {
+    
+    
+    // alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+    
+    
+    //} else {
+    
+    
+    // alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+    
+    
+    //}
